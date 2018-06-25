77a0d43c39e4c201040331d0bc87371251ccd95a
zhangminglu <1367000465@qq.com>
2018-06-25 14:59:22 +0800

    sd

--- 06-spider/day01/zhilian_search.py
+++ 06-spider/day01/zhilian_search.py
@@ -1,14 +1,25 @@
 
+import re
+
 import urllib.request
 from urllib import parse
 
 
-def get_zhilian_html(url):
-
+def get_zhilian_html(url, search):
+    url = url + search
     header = {
-        'User-Agent': ''
+        'User-Agent': r'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
     }
-    
+    req = urllib.request.Request(url=url, headers=header)
+    res = urllib.request.urlopen(req)
+
+    return res.read().decode('utf-8')
+
+
+def get_job_num(html):
+    num = re.
+    return
+
 
 def main():
 
@@ -16,7 +27,8 @@ def main():
     job = input('搜索的岗位：')
     search = parse.urlencode({'jl':city, 'kw': job})
     url = 'https://'
-    get_zhilian_html()
+    html = get_zhilian_html(url, search)
+
 
 
 if __name__ == '__main__':
