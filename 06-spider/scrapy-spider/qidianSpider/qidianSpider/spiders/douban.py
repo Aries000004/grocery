c278ecc5920ee799f7e1eeb12a6a96ab201bfd6a
zhangminglu <1367000465@qq.com>
2018-07-02 19:27:02 +0800

    scrapy爬虫框架

---
+++ 06-spider/scrapy-spider/qidianSpider/qidianSpider/spiders/douban.py
@@ -0,0 +1,48 @@
+
+import scrapy
+from scrapy.selector import Selector
+
+from qidianSpider.items import DoubanspiderItem
+
+from utils.clearn_data import handle
+
+
+class DouBanSpider(scrapy.Spider):
+
+    name = 'douban'
+
+    def start_requests(self):
+
+        for i in range(10):
+            url = r'https://movie.douban.com/top250?start=%s&filter=' % i * 25
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+        res = Selector(response)
+        items = DoubanspiderItem()
+        # 电影名
+        items['movie_name'] = res.xpath('//*[@id="content"]/div/div[1]/ol/li/div/div[2]/div[1]/a/span[1]/text()').extract()
+        # 图片
+        items['img'] = res.xpath('//*[@id="content"]/div/div[1]/ol/li/div/div[1]/a/img/@src').extract()
+
+        # 导演演员信息
+        director = res.xpath('//*[@id="content"]/div/div[1]/ol/li/div/div[2]/div[2]/p[1]/text()[1]').extract()
+        items['director'] = [handle(info) for info in director]
+
+        # 年份分类信息
+        year_type = res.xpath('//*[@id="content"]/div/div[1]/ol/li/div/div[2]/div[2]/p[1]/text()[2]').extract()
+        y_c_t_info = [handle(s).split('/') for s in year_type]
+        year, country, type = [], [], []
+        for y_c_t in y_c_t_info:
+            year.append(y_c_t[0])
+            country.append(y_c_t[1])
+            type.append(y_c_t[2])
+        items['year'] = year
+        items['country'] = country
+        items['type'] = type
+
+        # 评分
+        items['rate'] = res.xpath('//*[@id="content"]/div/div[1]/ol/li/div/div[2]/div[2]/div/span[2]/text()').extract()
+
+        return items
